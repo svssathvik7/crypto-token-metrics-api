66a2e24cd65bc33a0725f1ac8453a9744364da8d
SATHVIK SVS <svssathvik77@gmail.com>
2024-10-22 10:09:54 +0530

    func: setup http server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{self, App, HttpServer};
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()>{
+    HttpServer::new(
+        || {
+            App::new()
+        }
+    ).bind("localhost:3000")?.run().await
 }
