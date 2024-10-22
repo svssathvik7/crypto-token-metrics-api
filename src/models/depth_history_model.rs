d71962f5446cf170dda42d7eaeb48c00d68de3b5
SATHVIK SVS <svssathvik77@gmail.com>
2024-10-22 11:06:49 +0530

    major: model pool depth price history

--- src/models/depth_history_model.rs
+++ src/models/depth_history_model.rs
@@ -1,3 +1,18 @@
+use mongodb::bson::oid::ObjectId;
+
 pub struct PoolDepthPriceHistory{
-    
+    pub _id : ObjectId,
+    pub name : String,
+    pub asset_depth : i64,
+    pub asset_price : f64,
+    pub asset_price_usd : f64,
+    pub end_time : i64,
+    pub liquidity_units : i64,
+    pub luvi : f64,
+    pub members_count : i32,
+    pub rune_depth : i64,
+    pub start_time : i64,
+    pub synth_supply : i64,
+    pub synth_units : i64,
+    pub units : i64
 }
