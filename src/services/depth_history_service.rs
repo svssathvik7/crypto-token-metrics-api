9ed2fe4f7795236f833c1c0d2e0d4acbd6b5b965
SATHVIK SVS <svssathvik77@gmail.com>
2024-10-22 12:00:22 +0530

    major: Interval struct for ApiResponse

--- src/services/depth_history_service.rs
+++ src/services/depth_history_service.rs
@@ -2,18 +2,37 @@ use serde::{Deserialize, Serialize};
 
 #[derive(Debug,Deserialize,Serialize)]
 pub struct Meta {
-    pub end_asset_depth: String,
-    pub end_lp_units: String,
-    pub end_member_count: String,
-    pub end_rune_depth: String,
-    pub end_synth_units: String,
-    pub end_time: String,
-    pub luvi_increase: String,
-    pub price_shift_loss: String,
-    pub start_asset_depth: String,
-    pub start_lp_units: String,
-    pub start_member_count: String,
-    pub start_rune_depth: String,
-    pub start_synth_units: String,
-    pub start_time: String,
+    // couldnt follow snake case since that how its in api response of midgard
+    pub endAssetDepth: String,
+    pub endLPUnits: String,
+    pub endMemberCount: String,
+    pub endRuneDepth: String,
+    pub endSynthUnits: String,
+    pub endTime: String,
+    pub luviIncrease: String,
+    pub priceShiftLoss: String,
+    pub startAssetDepth: String,
+    pub startLPUnits: String,
+    pub startMemberCount: String,
+    pub startRuneDepth: String,
+    pub startSynthUnits: String,
+    pub startTime: String,
 }
+
+#[derive(Debug,Serialize,Deserialize)]
+pub struct Interval {
+    // couldnt follow snake case since that how its in api response of midgard
+    pub assetDepth: String,
+    pub assetPrice: String,
+    pub assetPriceUSD: String,
+    pub endTime: String,
+    pub liquidityUnits: String,
+    pub luvi: String,
+    pub membersCount: String,
+    pub runeDepth: String,
+    pub startTime: String,
+    pub synthSupply: String,
+    pub synthUnits: String,
+    pub units: String,
+}
+
