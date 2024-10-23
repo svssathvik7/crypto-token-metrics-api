c200a15c5e175cd94a818be3dbf4f08ff472031f
SATHVIK SVS <svssathvik77@gmail.com>
2024-10-23 12:10:58 +0530

    major: create fetch all rune history route

--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -1,3 +1,4 @@
 pub mod depth_route;
 pub mod earning_route;
 pub mod swap_route;
+pub mod rune_pool_route;
---
+++ src/routes/rune_pool_route.rs
@@ -0,0 +1,26 @@
+use actix_web::{web, HttpResponse, Responder};
+use chrono::Utc;
+
+use crate::{models::rune_pool_model::RunePool, services::db::DataBase};
+// Protected route
+// Expensive function
+#[actix_web::get("/fetch-rune-pools-all")]
+async fn fetch_all_rune_pools_to_db(db:web::Data<DataBase>) -> impl Responder{
+    let current_time_stamp = Utc::now().timestamp();
+    // epoch value of the api's start derived from the midgard metadata
+    let mut start = 1647913096;
+    loop{
+        let end_time = match RunePool::fetch_rune_pool(&db, "hour", "400", &start.to_string()).await {
+            Ok(response) => response,
+            Err(e) => {
+                println!("Failed to fetch and update db with rune pool history! {:?}\n",e);
+                current_time_stamp+10
+            }
+        };
+        if end_time >= current_time_stamp{
+            break;
+        }
+        start = end_time;
+    }
+    HttpResponse::Ok().body(format!("Fetched and added rune pool records to database"))
+}
