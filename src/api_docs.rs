a70945c2d3c625c439fca27b4861649fbf57c4aa
SATHVIK SVS <svssathvik77@gmail.com>
2024-10-25 18:30:47 +0530

    major: update docs title in swagger ui

--- src/api_docs.rs
+++ src/api_docs.rs
@@ -2,6 +2,11 @@ use utoipa::OpenApi;
 
 #[derive(OpenApi)]
 #[openapi(
+    info(
+        title = "crypto-token-metrics-api",
+        description = "The API serves as a replicated interface to the Midgard Public API, providing accessible information about the THORChain network and associated chains via Bifröst. By querying the Midgard API, it fetches and processes transaction event data, storing it in a structured format that supports time-series analysis for easy, time-dependent queries. While this API makes THORChain network data available, critical protocol interactions should still be performed directly with THORNode for access to primary blockchain data.",
+        version = "1.0.0"
+    ),
     paths(
         crate::routes::depth_route::get_depth_price_history,
         crate::routes::swap_route::get_swaps_history,
